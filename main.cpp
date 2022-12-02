3dc9a5ea3823fd504de41057b4e80f6a3559d1be
5612466 <115354030+5612466@users.noreply.github.com>
2022-12-02 16:39:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std; 
 
 int main(){
+  int k;
+  cin >> k;
+  if (numeroprimo(k)==0){
+    cout << "numero primo" << endl;
+  }
+  else {
+    cout << "numero non primo" << endl;
+  }
   return 0;
 }
